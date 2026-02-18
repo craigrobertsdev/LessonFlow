bb4fcf33c630d800397dc61c3ae4f9e15ea8e82a
Craig Roberts <craig.roberts11@outlook.com>
2026-02-19 05:05:22 +1030

    Updates start up script

--- start-services.sh
+++ start-services.sh
@@ -2,6 +2,3 @@
 
 echo "Starting Azurite"
 sudo docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 -v /home/craig/.local/azurite mcr.microsoft.com/azure-storage/azurite &
-
-echo "Starting Open Project" 
-sudo docker run -i -p 5500:80 -e SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=localhost:8080 -e OPENPROJECT_HTTPS=false -e OPENPROJECT_DEFAULT__LANGUAGE=en openproject/openproject:dev &
